038ac434fbcd8e47fe624dc14e098bc54585c3a6
Yoan Smit <yoan.smit@gmailcom>
2022-11-12 19:28:25 +0100

    fix: URL of the repo

--- Sources/CodingWithWolves/main.swift
+++ Sources/CodingWithWolves/main.swift
@@ -31,7 +31,7 @@ let indentation: Indentation.Kind? = nil
 let additionalSteps: [PublishingStep<CodingWithWolves>] = []
 let rssFeedSections: Set<CodingWithWolves.SectionID> = Set(CodingWithWolves.SectionID.allCases)
 let rssFeedConfig: RSSFeedConfiguration? = .default
-let deploymentMethod: DeploymentMethod<CodingWithWolves>? = .gitHub("Naxyoh/naxyoh.github.io.git", useSSH: true)
+let deploymentMethod: DeploymentMethod<CodingWithWolves>? = .gitHub("Naxyoh/naxyoh.github.io", useSSH: true)
 
 try CodingWithWolves()
     .publish(
